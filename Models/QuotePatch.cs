717df561fdce78750c7bdcbbcaf313240ef67db1
tim <tim@ioplus.de>
2024-01-23 17:34:27 +0100

    #8 init Patch datatsructure with QuotePatch Object

---
+++ Models/QuotePatch.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class QuotePatch
+{
+
+    public QuotePatch(){}
+}
+/*
+    // Patch: quotes
+    [HttpPatch]
+    public ActionResult<Object> Patch(QuotePatch quotePatch)
+    {
+        
+    }
+*/
